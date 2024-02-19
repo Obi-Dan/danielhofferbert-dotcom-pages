9917752a6bd9c301df87d128b98df4bde9ab16ee
Obi-Dan <danshoff@gmail.com>
2024-02-19 12:33:21 +0000

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coming soon...
+          WIP - Coming soon...
         </p>
         <a
           className="App-link"
