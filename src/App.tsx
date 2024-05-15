8e1a22e1bf790365e419e2e438910905d1f96da4
Obi-Dan <danshoff@gmail.com>
2024-05-14 21:04:53 -0500

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          WIP - Coming soon...
+          DanielHofferbert.com Coming soon...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Name name={'bob2'} />
+        {/* <Name name={'bob2'} /> */}
       </header>
     </div>
   );
